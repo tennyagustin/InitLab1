2094f9218c516c85d97b052b984ca969d57ef6bd
Tenny <Tenny@Zipcoder24s-MacBook-Pro.local>
2016-01-27 18:55:49 -0500

    created 5 BoringThings and 5 ColorfulThings

--- src/main/java/InitializationLab1.java
+++ src/main/java/InitializationLab1.java
@@ -1,6 +1,24 @@
+import agustin.tenny.initlab01.BoringThing;
+import agustin.tenny.initlab01.ColorfulThing;
+
 /**
  * Created by Tenny on 1/27/16.
  */
 public class InitializationLab1 {
+    public static void main(String[] args) {
+        BoringThing boringThing1 = new BoringThing();
+        BoringThing boringThing2 = new BoringThing();
+        BoringThing boringThing3 = new BoringThing();
+        BoringThing boringThing4 = new BoringThing();
+        BoringThing boringThing5 = new BoringThing();
+
+        ColorfulThing colorfulThing1 = new ColorfulThing("blue");
+        ColorfulThing colorfulThing2 = new ColorfulThing("blue");
+        ColorfulThing colorfulThing3 = new ColorfulThing("blue");
+        ColorfulThing colorfulThing4 = new ColorfulThing("blue");
+        ColorfulThing colorfulThing5 = new ColorfulThing("blue");
+
+        System.out.println(colorfulThing1.getColor());
+    }
 
 }
---
+++ src/main/java/agustin/tenny/initlab01/BoringThing.java
@@ -0,0 +1,7 @@
+package agustin.tenny.initlab01;
+
+/**
+ * Created by Tenny on 1/27/16.
+ */
+public class BoringThing {
+}
